05eab07ee82d2afde3d385c49f38a99284c5984c
Vitor Souza <vitorrhss@gmail.com>
2017-02-07 21:46:12 -0200

    Utilizando get com promise implícito

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,11 +2,11 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
     
     $scope.fotos = [];
 
-    var promise = $http.get('v1/fotos');
-
-    promise.then(function(retorno){
-        $scope.fotos = retorno.data;
-    }).catch(function(error){
-        console.log(error)
+    $http.get('v1/fotos')
+    .success(function(fotos){
+        $scope.fotos = fotos;
+    })
+    .error(function(erro){
+        console.log(erro);
     });
 });
