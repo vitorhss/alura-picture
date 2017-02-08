a6ef6330950e472ce1878df2aa1854021d9df1a9
Vitor Souza <vitorrhss@gmail.com>
2017-02-08 16:03:38 -0200

    Criando uma diretiva

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,15 @@
+angular.module('minhasDiretivas', [])
+.directive('meuPainel', function(){
+    
+    var ddo = {};
+    ddo.restrict = 'AE';
+
+    ddo.scope = {
+        titulo: '@titulo'
+    };
+
+    ddo.transclude = true;
+
+    ddo.templateUrl  = 'js/directives/meu-painel.html';
+    return ddo;
+})
