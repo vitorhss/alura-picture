f23cb560b6be30543057f7566a027bd7d975370d
Vitor Souza <vitorrhss@gmail.com>
2017-02-08 17:55:31 -0200

    Utilizando a diretiva ng-model

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,6 +2,8 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
     
     $scope.fotos = [];
 
+    $scope.filtro = '';
+
     $http.get('v1/fotos')
     .success(function(fotos){
         $scope.fotos = fotos;
