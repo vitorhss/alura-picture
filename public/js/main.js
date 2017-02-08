f21127abbd72e583f5f3178ec4b2ae2bbf0b6079
Vitor Souza <vitorrhss@gmail.com>
2017-02-08 20:10:59 -0200

    Utilizando o sistema de rotas

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider){
+    
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/fotos', {
+        templateUrl :'partials/principal.html',
+        controller : 'FotosController'
+    });
+
+    $routeProvider.otherwise({redirectTo: '/fotos'
+    });
+});
